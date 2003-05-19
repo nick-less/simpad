7629d83959b21b93a58eb636161f1c322507002d
ppregler <>
2003-05-19 21:21:53 +0000

    Initial revision - linux-2.4.19-rmk7

---
+++ simpad-kernel/linux/include/asm-sparc64/head.h
@@ -0,0 +1,11 @@
+/* $Id$ */
+#ifndef _SPARC64_HEAD_H
+#define _SPARC64_HEAD_H
+
+#include <asm/pstate.h>
+
+#define KERNBASE	0x400000
+
+#define	PTREGS_OFF	(STACK_BIAS + REGWIN_SZ)
+
+#endif /* !(_SPARC64_HEAD_H) */
